cafd4ad1c6e0ff6bb29ee5830345464eda95af37
Kolyo1 <96702042+Kolyo1@users.noreply.github.com>
2025-02-17 13:25:30 +0200

    b

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include<stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int findMax(int, int);
 double square(double );
 void Birthday(char[],int);
 void hello(char[], int);//function protorype
+void Sort(int array[], int);
+struct Player
+{
+    char name[12];
+    int score;
+};
+//typedef char user[25];
+typedef struct{
+    char name[25];
+    char password[12];
+    int id;
+}User;
 
+struct Student{
+    char name[12];
+    float gpa;
+};
+
+enum Day{Sun = 1 ,Mon = 2,Tue = 3,Wed = 4,Thu = 5,Fri = 6,Sat = 7};
 
 int main(){
 
@@ -235,8 +255,8 @@ int main(){
 
     // string functions
 
-    char string1[] = "Bro";
-    char string2[] = "Code";
+    //char string1[] = "Bro";
+    //char string2[] = "Code";
 
     // strlwr(string1);   // converts a string to lowercase = (bro)
     // strupr(string2);   // converts a string to uppercase = (BRO)
@@ -293,8 +313,159 @@ int main(){
 
     // nested loop = a loop inside of another loop
     
+    // continue = skips rest of the code and forces the next iteration of the loop
+    // break = exits a lopp/switch
 
+    // array = a data structure that can store many values of the same data type.
+    // [] = to make an array [25] = to set size
+
+    // double prices[] = {5,6,7,9,12,24};
+
+    // for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++){
+    //     printf("$ %.1lf\n", prices[i]);
+    // }
+
+    // 2D array = an array, where each element is an entire array
+    // useful if you need a matrix, grid or table of data
+
+    // int numbers[2][3] = {
+    //                     {1,2,3},
+    //                     {4,5,6}
+    //                     };
+
+    // int numbers[3][3];
+    // int rows = sizeof(numbers)/sizeof(numbers[0]);
+    // int columns = sizeof(numbers[0])/sizeof(numbers[0][0]);
+    // printf("Rows: %d" , rows);
+    // printf("Columns: %d\n", columns);
+    // numbers[0][0] = 1;
+    // numbers[0][1] = 2;
+    // numbers[0][2] = 3;
+    // numbers[1][0] = 3;
+    // numbers[1][1] = 4;
+    // numbers[1][2] = 5;
+    // numbers[2][0] = 6;
+    // numbers[2][1] = 7;
+    // numbers[2][2] = 8;
+
+    // for(int i = 0; i<rows;i++){
+    //     for(int j = 0; j<columns; j++){
+    //         printf("%d ", numbers[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    //char cars[][10] = {"Mustand", "Corvette", "Camaro"};
+    // cars[0] = "Tesla"l  // не работи
+    // strcpy(cars[0], "Tesla");
+
+    // for(int i = 0; i< sizeof(cars)/sizeof(cars[0]); i++){
+    //     printf("%s\n", cars[i]);
+    // }
+
+    // char x[15] = "Water";
+    // char y[15] = "soda";
+    // char temp[15];
+
+    // strcpy(temp,x);
+    // strcpy(x,y);
+    // strcpy(y,temp);
+    
+    // printf("X: %s\n", x);
+    // printf("Y: %s\n", y);
+
+    // array sort
+
+    //int array[] = {9,2,3,4,8,6,7,1,5};
+    // char array[] = {"A","B","C"};
+    // int size = sizeof(array)/sizeof(array[0]);
+    // Sort(array,size);
+    // PrintArray(array,size);
+
+    // struct = collection of related members("variables")
+    // they can be of different data types
+    // listed under one name in a block of memory
+    // VERY SIMILAR to classes in other languages( but not methods)
+
+    // struct Player player1;
+    // struct Player player2;
+
+    // strcpy(player1.name, "Ahmed");
+    // player1.score = 4;
+    // strcpy(player2.name, "Muhamed");
+    // player2.score = 5;
+
+    // printf("Name: %s\n", player1.name);
+    // printf("Score: %d\n", player1.score);
+    // printf("Name: %s\n", player2.name);
+    // printf("Score: %d\n", player2.score);
+
+    // typedef = reversed keyword that gives an existing datatype a "nickname"  
+    // User user1 = {"Bro", "password123", 1};
+    // User user2 = {"Bruh", "password321", 2};
+
+    // printf("%s\n", user1.name);
+    // printf("%s\n", user1.password);
+    // printf("%d\n", user1.id);
+    // printf("\n");
+    // printf("%s\n", user2.name);
+    // printf("%s\n", user2.password);
+    // printf("%d\n", user2.id);
+
+    // struct Student student1 = {"Spongebob", 3.0};
+    // struct Student student2 = {"Sandy", 6.0};
+    // struct Student student3 = {"Patrick", 2.5};
+    // struct Student students[] = {student1,student2,student3};
+    // for(int i =0; i< sizeof(students)/sizeof(students[0]); i++){
+    //     printf("%-12s\t", students[i].name);
+    //     printf("%.2f\n", students[i].gpa);
+    // }
+
+    // enum = a user defined type of named integer identifiers
+    // helps to make a program more readable
+    // enums are NOT STRING, but they can be treated as int
+    // enum Day today = Sun;
+    // if(today == Sun || today == Sat){
+    //     printf("Weekend. party time\n");
+    // }
+    // else
+    // {
+    //     printf("I have to work\n");
+    // } 
+
+    //pseudo random numbers = A set of values or elements that are statistically random
+    // (Don't use these for any sort of cryptographic security)
+    // #include <stdlib.h>
+    // #include <time.h>
+    // srand(time(0));
+    // int number1 = (rand() % 6) + 1;
+    // int number2 = (rand() % 6) + 1;
+    // int number3 = (rand() % 6) + 1;
+    // printf("%d\n", number1);
+    // printf("%d\n", number2);
+    // printf("%d\n", number3);
+
+    
     return 0;
+}
+
+
+void PrintArray(int array[], int size){
+    for(int i = 0; i <size; i++){
+        printf("%c", array[i]);
+    }
+}
+
+void Sort(int array[], int size){
+    for(int i = 0; i < size-1; i++){
+        for(int j = 0; j < size-1; j++){
+            if(array[j] > array[j+1]){
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
 }
 
 int findMax(int x, int y)
